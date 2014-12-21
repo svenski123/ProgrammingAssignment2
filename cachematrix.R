83d06f29f168a2f4788a3b8d0f7510d4d4a7cf2a
Kristofer Peterson <kris@svenski.net>
2014-12-21 21:55:18 +0000

    Added cached matrix routines.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cached Matrix Functions
 
-## Write a short comment describing this function
+## Routines for creating and manipulating cached matrix objects
 
+## makeCacheMatrix takes a matrix and returns a cached matrix object
+## currently implemented as a list of four functions with a shared
+## environment storing both the matrix and possibly its cached inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		inverse <<- NULL
+	}
+	get <- function() x
+    setinverse <- function(i) inverse <<- i
+	getinverse <- function() inverse
+	list(set = set,
+	     get = get,
+	     setinverse = setinverse,
+	     getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve takes a cached matrix object (as created by makeCacheMatrix)
+## and returns the inverse. Upon the invocation, the inverted matrix is
+## cache and returned directly on subsequent invocations.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	i <- x$getinverse()
+	if (is.null(i)) {
+		i <- solve(x$get())
+		x$setinverse(i)
+	}
+	i
 }
